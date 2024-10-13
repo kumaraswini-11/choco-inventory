5d7c20f6cf95881923d335fad8d3787e974fcba1
kumaraswini-11 <kumaraswini.11.11@gmail.com>
2024-10-13 15:26:40 +0530

    Step 2: Project Configuration

---
+++ src/config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Choco",
+  description:
+    "Choco Inventory is your go-to full-stack e-commerce platform for premium chocolates, featuring seamless shopping and advanced inventory management. Indulge in a delightful chocolate experience today!",
+};
